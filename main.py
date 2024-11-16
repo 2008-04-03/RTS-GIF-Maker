206a8aa34115e70c8e700e33ea198cb8b3dbf710
YuYu1015 <whes1015@gmail.com>
2024-11-17 05:34:45 +0800

    fix: crash

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 import shutil
 
 date_start = "2024-11-16 17:30:40"
-date_end = "2024-11-16 17:35:00"
+date_end = "2024-11-16 17:30:45"
 
 datetime_start = datetime.strptime(date_start, "%Y-%m-%d %H:%M:%S")
 datetime_end = datetime.strptime(date_end, "%Y-%m-%d %H:%M:%S")
@@ -18,7 +18,7 @@ timestamp_ms_end = int(datetime_end.timestamp() * 1000)
 
 raw = Image.open("./rts-image.png")
 
-_t=round((timestamp_ms_end - timestamp_ms_start) / 1000)
+_t = round((timestamp_ms_end - timestamp_ms_start) / 1000)
 
 if os.path.exists("./images"):
     shutil.rmtree("./images")
@@ -29,7 +29,7 @@ if not os.path.exists("./images"):
 with tqdm(total=_t, desc="進度") as pbar:
     for t in range(_t):
         response = requests.get(
-            "https://api-2.exptech.dev/api/v1/trem/rts-image?time={}".format(timestamp_ms_start + t * 1000))
+            f"https://api-2.exptech.dev/api/v1/trem/rts-image?time={timestamp_ms_start + t * 1000}")
         if response.status_code == 200:
             result_image = raw.copy()
             image_data = BytesIO(response.content)
@@ -37,19 +37,49 @@ with tqdm(total=_t, desc="進度") as pbar:
             variable_img = variable_img.convert("RGBA")
             result_image = Image.alpha_composite(result_image, variable_img)
             result_image.save(
-                "./images/{}.png".format(timestamp_ms_start + t * 1000))
+                f"./images/{timestamp_ms_start + t * 1000}.png", format="PNG")
         else:
             print(
                 f"Failed to download image at timestamp {timestamp_ms_start + t * 1000}")
         time.sleep(0.5)
         pbar.update(1)
 
-file_list = os.listdir("./images")
-images = []
+def prepare_for_gif(im):
+    rgba = im.convert('RGBA')
+    alpha = rgba.split()[3]
+    rgb = rgba.convert('RGB')
+    p_img = rgb.convert('P', palette=Image.ADAPTIVE, colors=255)
+    p_img.info['transparency'] = 255
+    mask = Image.eval(alpha, lambda a: 255 if a < 128 else 0)
+    p_img.paste(255, mask)
+    
+    return p_img
 
-for filename in file_list:
-    img = Image.open("./images/{}".format(filename))
-    images.append(img)
+def create_gif(image_folder, output_path):
+    file_list = sorted(os.listdir(image_folder))
+    images = []
+    
+    for filename in file_list:
+        img_path = os.path.join(image_folder, filename)
+        img = Image.open(img_path)
+        prepared_img = prepare_for_gif(img)
+        images.append(prepared_img)
+    
+    try:
+        images[0].save(
+            output_path,
+            save_all=True,
+            append_images=images[1:],
+            duration=100,
+            loop=0,
+            transparency=255,
+            disposal=2
+        )
+        print(f"GIF successfully created at {output_path}")
+    except Exception as e:
+        print(f"Error creating GIF: {e}")
+        raise
+    
+    return output_path
 
-images[0].save("output.gif", save_all=True,
-               append_images=images[1:], duration=100, loop=0, disposal=2)
+create_gif("./images", "output.gif")
